422b8e7e95e0b0e2e349f19f3c752b118b1f47c3
eviass19 <61251246+eviass19@users.noreply.github.com>
2020-03-22 17:07:32 +0300

    Update Project_0 trial 2.py
    11

--- Project_0 trial 2.py
+++ Project_0 trial 2.py
@@ -29,17 +29,16 @@ if s==False:
     print('Not relevant number')
 else:
     print('Number is', s, ' Qty of trials is', count)
-    
+
 def score_game(bsearch):
     #'''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
     random_array = np.random.randint(1, 100, size=(1000))
     for i in random_array:
-        count_ls.append(bsearch(number,guess,x,y))
+        count_ls.append(count)
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return(score)
  
 score_game(bsearch)
-
